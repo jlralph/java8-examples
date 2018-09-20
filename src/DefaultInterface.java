2624cc6f26fc6748af320b41bcaa2fb6a9c12c4d
jlralph <josh.l.ralph@gmail.com>
2018-09-20 16:22:17 -0700

    Added a comment string to the method

--- src/DefaultInterface.java
+++ src/DefaultInterface.java
@@ -1,6 +1,8 @@
 import java.util.UUID;
 
 public interface DefaultInterface {
+	
+	// just demonstrating a default method in an interface.
 	default public String getUUID() {
 		return UUID.randomUUID().toString();
 	}
