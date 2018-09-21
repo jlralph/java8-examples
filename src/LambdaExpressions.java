e6caeee6012cc4cd56777a6c405c23b1f81384b0
Josh Ralph <josh.l.ralph@gmail.com>
2018-09-21 13:19:00 -0700

    Added method comment

--- src/LambdaExpressions.java
+++ src/LambdaExpressions.java
@@ -12,6 +12,11 @@ public class LambdaExpressions {
 
 	private Comparator<String> stringComparator = ((string1, string2) -> { return string1.compareTo(string2); });
 
+	/**
+	 * @param stringOne
+	 * @param stringTwo
+	 * @return 0 if the two strings are equal
+	 */
 	public int compareStringsWithLambda(String stringOne, String stringTwo) {
 		return stringComparator.compare(stringOne, stringTwo);
 	}
