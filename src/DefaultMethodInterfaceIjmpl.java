0020d655d3a090812938557adc024afada607ff5
Josh Ralph <josh.l.ralph@gmail.com>
2018-09-21 13:43:13 -0700

    Changes to call lambda instead of returning static string.

--- src/DefaultMethodInterfaceIjmpl.java
+++ src/DefaultMethodInterfaceIjmpl.java
@@ -1,21 +1,23 @@
 /**
- * 
- */
-
-/**
  * @author Josh Ralph
  * 
  * Example implementation class that implements an interface that contains a default function
  *
  */
 public class DefaultMethodInterfaceIjmpl implements DefaultMethodInInterface {
-
+	
+	DefaultMethodInInterface dmi = () -> "joshr";
+	
+	private String getUserNameUsingLambda() {
+		return dmi.getUsername();
+	}
+	
 	/* (non-Javadoc)
 	 * @see DefaultMethodInInterface#getUsername()
 	 */
 	@Override
 	public String getUsername() {
-		return "joshr";
+		return getUserNameUsingLambda();
 	}
 
 }
